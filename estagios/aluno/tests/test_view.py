77fdb16f695fe9ba274319db92a1fa10199086f8
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2019-06-10 10:44:26 -0300

    Refatorar os testes #3

---
+++ estagios/aluno/tests/test_view.py
@@ -0,0 +1,173 @@
+from django.shortcuts import resolve_url as r
+from django.test import Client, TestCase
+
+from estagios.core.models import User
+
+view_template_list = [
+    ('aluno:aluno_cadastro_dados_pessoais',
+     'aluno_cadastro.html'),
+
+    ('aluno:aluno_home',
+     'aluno_index.html'),
+
+    ('aluno:aluno_esqueceu_senha',
+     'aluno_cadastro.html')
+]
+
+
+class GetRedirectTest(TestCase):
+    def setUp(self):
+        self.resp_list = []
+        for view_test in view_template_list:
+            self.resp_list.append(
+                self.client.get(r(view_test[0]))
+            )
+
+    def test_302_response(self):
+        for response in self.resp_list:
+            with self.subTest():
+                self.assertEqual(302, response.status_code)
+
+
+class GetRedirectTestFollow(TestCase):
+    def setUp(self):
+        self.resp_list = []
+        for view_test in view_template_list:
+            self.resp_list.append(
+                self.client.get(r(view_test[0]), follow=True)
+            )
+
+    def test_200_response(self):
+        for response in self.resp_list:
+            with self.subTest():
+                self.assertEqual(200, response.status_code)
+
+    def test_redirect_url(self):
+        for response in self.resp_list:
+            with self.subTest():
+                self.assertEqual(
+                    '/alunos/login/',
+                    response.redirect_chain[0][0]
+                )
+
+    def test_template_used(self):
+        for response in self.resp_list:
+            with self.subTest():
+                self.assertTemplateUsed(
+                    response,
+                    'aluno_login.html'
+                )
+
+
+class AlunoGet(TestCase):
+    def setUp(self):
+        self.client = Client()
+        User.objects.create_user(
+            'admin',
+            'admin@admin.com',
+            '123',
+            is_student=True)
+        self.client.login(username='admin', password='123')
+        self.resp_list = []
+        for view_test in view_template_list:
+            self.resp_list.append((
+                self.client.get(r(view_test[0])),
+                view_test[1]
+            ))
+
+    def test_template(self):
+        for response in self.resp_list:
+            with self.subTest():
+                self.assertTemplateUsed(
+                    response[0],
+                    response[1]
+                )
+
+    def test_200_template(self):
+        for response in self.resp_list:
+            with self.subTest():
+                self.assertEqual(
+                    200,
+                    response[0].status_code
+                )
+
+
+class NotAlunoGet(TestCase):
+    def setUp(self):
+        self.client = Client()
+        User.objects.create_user(
+            'admin',
+            'admin@admin.com',
+            '123',
+            is_student=False)
+        self.client.login(username='admin', password='123')
+        self.resp_list = []
+        for view_test in view_template_list:
+            self.resp_list.append(
+                self.client.get(r(view_test[0]), follow=True)
+            )
+
+    def test_302_template(self):
+        for response in self.resp_list:
+            with self.subTest():
+                self.assertTrue(
+                    302,
+                    response.status_code
+                )
+
+
+class NotAlunoGetFollow(TestCase):
+    def setUp(self):
+        self.client = Client()
+        User.objects.create_user(
+            'admin',
+            'admin@admin.com',
+            '123',
+            is_student=False)
+        self.client.login(username='admin', password='123')
+        self.resp_list = []
+        for view_test in view_template_list:
+            self.resp_list.append(
+                self.client.get(r(view_test[0]), follow=True)
+            )
+
+    def test_200_template(self):
+        for response in self.resp_list:
+            with self.subTest():
+                self.assertTrue(
+                    200,
+                    response.status_code
+                )
+
+    def test_redirect_url(self):
+        for response in self.resp_list:
+            with self.subTest():
+                self.assertTrue(
+                    '/',
+                    response.redirect_chain[0][0]
+                )
+
+    def test_template_used(self):
+        for response in self.resp_list:
+            with self.subTest():
+                self.assertTemplateUsed(
+                    response,
+                    'index.html'
+                )
+
+
+class AnonymousGet(TestCase):
+    def setUp(self):
+        self.resp_list = []
+        for view_test in view_template_list:
+            self.resp_list.append(
+                self.client.get(r(view_test[0]), follow=True)
+            )
+
+    def test_302_template(self):
+        for response in self.resp_list:
+            with self.subTest():
+                self.assertTrue(
+                    302,
+                    response.status_code
+                )
--- estagios/aluno/views.py
+++ estagios/aluno/views.py
@@ -24,13 +24,12 @@ def cadastro_dados_pessoais(request):
     context = {}
     return render(request, 'aluno_cadastro.html', context)
 
-
 @area_student
 def home(request):
     context = {}
     return render(request, 'aluno_index.html', context)
 
-
+@area_student
 def esqueceu_senha(request):
     context = {}
     return render(request, 'aluno_cadastro.html', context)
